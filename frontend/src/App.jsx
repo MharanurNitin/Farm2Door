52f95724ced112d266aae5c538a371368b0597d4
MharanurNitin <nitinmharanur@gmail.com>
2023-03-26 12:18:33 +0530

    frontend vite app created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-400 text-lg font-bold text-center w-full m-auto">
+      hello world
+    </div>
+  );
+}
+
+export default App;
